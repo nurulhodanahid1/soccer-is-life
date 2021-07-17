3b4ed6c9bb685b418c7e0d01fd3631b2f88a9c9c
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-07-17 21:46:06 +0600

    f

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import LeagueDetails from './components/LeagueDetails/LeagueDetails';
